54a5d322bbe60f42ebdd1befd731b03091598af7
AnikBarua <anikbaruanirob@gmail.com>
2018-10-08 03:35:49 +0000

    task 4 and task 5

--- script.js
+++ script.js
@@ -13,11 +13,41 @@ function sumArray(arr){
 }
 console.log(sumArray([2,4,5,6,7]))
 
+
+//task - 3
+
 function checkEmail(emailstring){
     var emailformat = /^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/;
     var match = emailformat.test(emailstring);
     return match;
 }
-console.log(checkEmail("jhon@smith.com "));
+console.log(checkEmail("jhon@smith.com"));
 console.log(checkEmail("jhonsmith.com"));
+
+//task-4
+
+var library = [ 
+   {
+       author: 'Bill Gates',
+       title: 'The Road Ahead',
+       readingStatus: true
+   },
+   {
+       author: 'Steve Jobs',
+       title: 'Walter Isaacson',
+       readingStatus: true
+   },
+   {
+       author: 'Suzanne Collins',
+       title:  'Mockingjay: The Final Book of The Hunger Games', 
+       readingStatus: false
+   }];
+
+ function getReadingStatus(index){
+     var book = library[index];
+     return book.readingStatus;
+ }
+ console.log(getReadingStatus(1));
+ console.log(getReadingStatus(0));
+ console.log(getReadingStatus(2));
 

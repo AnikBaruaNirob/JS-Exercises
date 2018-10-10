2d3d1f36b76c4e26dc02ce9ea62d362c7532863e
AnikBarua <anikbaruanirob@gmail.com>
2018-10-10 03:14:08 +0000

    task 4 and task 5

--- script.js
+++ script.js
@@ -51,3 +51,69 @@ var library = [
  console.log(getReadingStatus(0));
  console.log(getReadingStatus(2));
 
+   var cart = [ 
+   {
+       name: 'Shoes',
+       price: 560,
+       quantity: 4
+   },
+   {
+       name: 'Regular Tees',
+       price: 455.50,
+       quantity: 6
+   },
+   {
+       name: 'Socks',
+       price: 65.99,
+       quantity: 2
+   }];
+function addItem (newItem){
+    cart.push (newItem);
+    console.log(cart);
+    
+    var newItem = {
+        name: 'watch',
+        price: 64,
+        quantity:1,
+        
+    };
+    
+    addItem(newItem);
+    //task5b
+   function sortCart(){
+  //add code
+  var len = cart.length;
+  var temp;
+  for(var i = 0; i <= len - 1; ++i) {
+    for (var j = 0; j <= len - 1; ++j) {
+      if(cart[i].name < cart[j].name) {
+        temp = cart[i];
+        cart[i] = cart[j];
+       cart[j] = temp;
+      }
+    }
+  }
+  return cart;
+}
+
+console.log(sortCart());
+        
+    }
+    //task5
+    function findByName(givenName){
+  for (var i = 0; i < cart.length; i++) {
+    if(cart[i].name == givenName){
+      console.log("Name: "+cart[i].name + "Price: " + cart[i].price + "Quantity: "+cart[i].quantity);
+    }
+  }
+}
+    //task5d
+    function totalBill(){
+  var totalAmount=0;
+  for( var i =0; i < cart.length; i++){
+    totalAmount = totalAmount + cart[i].quantity*cart[i].price;
+
+  }
+  return totalAmount;
+  }
+console.log(totalBill());
